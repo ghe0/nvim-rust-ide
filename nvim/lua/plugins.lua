17498a7385934a114cc09bd1b647220646862a39
ghe0 <gheorghe@gheo.tech>
2024-07-16 05:24:32 +0300

    switch from nvim tree and to telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,22 +2,22 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  use 'kyazdani42/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
   use 'airblade/vim-gitgutter'
   use 'rust-lang/rust.vim'
-  use 'rvmelkonian/move.vim'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
   use 'onsails/lspkind.nvim'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-buffer'
-  use 'jacoborus/tender.vim'
   use 'vyperlang/vim-vyper'
-  use 'nvim-tree/nvim-tree.lua'
   use 'ellisonleao/gruvbox.nvim'
   use 'j-hui/fidget.nvim'
   use 'savq/melange-nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'}, { 'BurntSushi/ripgrep'} }
+  }
 end)
 
